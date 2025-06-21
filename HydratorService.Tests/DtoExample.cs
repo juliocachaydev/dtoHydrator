c094a782f62daf26a06d8bdf0f9bf8e8f27c9dfd
juliocachaydev <julio.cachay.dev@gmail.com>
2025-06-21 13:49:38 -0400

    Wrote Tests, gonna clean-up

---
+++ HydratorService.Tests/DtoExample.cs
@@ -0,0 +1,8 @@
+﻿namespace HydratorService.Tests;
+
+public class DtoExample : HighPriorityHydraterExample.IHighPriorityExample, LowPriorityHydraterExample.ILowPriorityExample
+{
+    public decimal Dividend { get; set; }
+    public decimal Divisor { get; set; }
+    public decimal Quotient { get; set; }
+}
